7210186f7e16ea20ccad7cbcd7e410615ead9d70
evgeniy <evgeniy@takeit.to>
2016-03-13 21:53:10 +0200

    add methods to instance

--- instance/index.js
+++ instance/index.js
@@ -3,17 +3,53 @@
 const config = require('config');
 const md5 = require('md5')
 
+const SERVER_TYPE_GENERATOR = 1
+const SERVER_TYPE_WORKER = 2
+
+
 module.exports = class {
 
-    constructor(){
+    constructor(type){
         this.startAt = new Date().getTime();
         this.serverId = this.generateServerId();
+        switch (type) {
+            case 1:
+                this.serverId = 1;
+                break;
+            case 2:
+                this.serverId = 2;
+                break;
+            default:
+                this.serverId = 2;
+                break;
+        }
+
     }
 
     generateServerId(){
         let timestamp = new Date().getTime();
         let serverId = md5 (timestamp + config.secretKey);
             return serverId;
+    }
+
+    defineAsGenerator(){
+        this.serverType = 1;
+    }
+
+    isGenerator(){
+        if(this.serverType == 1){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    isWorker(){
+        if(this.serverType == 1){
+            return false;
+        }else {
+            return true;
+        }
     }
 
     getMessage(){
