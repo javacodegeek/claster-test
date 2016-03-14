64de1396b512468028220be6e4ac95d91641cc47
evgeniy <evgeniy@takeit.to>
2016-03-14 19:59:08 +0200

    complete handling message

--- server.js
+++ server.js
@@ -13,29 +13,35 @@ client.on("error", function (err) {
 
 let serverInstance = new Instance(2);
 
-
-
 if(serverInstance.isGenerator()){
-    let timerMessage = setInterval(function() {
-        let messageBody = { "serverId": serverInstance.serverId,
-                            "timestamp": new Date().getTime().toString(),
-                            "body": serverInstance.getMessage().toString(),
-                          };
+    let generatorInfo = { "serverId": serverInstance.serverId,
+                          "timestamp": new Date().getTime().toString(),
+    };
 
-        let messageKey = "messageKey-" + serverInstance.serverId + '-' + new Date().getTime();
+    client.HMSET("generator-info", messageBody, function(err, res){
+        if (!err) {
+            let timerMessage = setInterval(function() {
+                let messageBody = { "serverId": serverInstance.serverId,
+                    "timestamp": new Date().getTime().toString(),
+                    "body": serverInstance.getMessage().toString(),
+                };
 
-        client.HMSET(messageKey, messageBody, function(err, res){
-            console.log(err);
-        });
-    }, config.messageDelay);
+                let messageKey = "messageKey-" + serverInstance.serverId + '-' + new Date().getTime();
+
+                client.HMSET(messageKey, messageBody, function(err, res){
+                });
+            }, config.messageDelay);
+        }
+    });
 }else {
+
+
     client.keys("messageKey*", function (err, replies) {
         let hkeysList = replies;
-        console.log(hkeysList.length);
+
         for (let hkey of hkeysList) {
             let new_hkey = "lock-" + hkey;
             client.rename(hkey, new_hkey, function (err, res) {
-                console.log(err);
                 if (!err) {
                     client.hgetall(new_hkey, function (err, res) {
                         let message = res.body;
@@ -43,9 +49,11 @@ if(serverInstance.isGenerator()){
                             serverInstance.eventHandler(message, function (err, msg) {
                                 if (!err){
                                     let err_new_hkey = "error-" + new_hkey;
+                                    client.rename(new_hkey, err_new_hkey, function (err, res) {});
                                     console.log(err_new_hkey);
                                 }else {
                                     let suc_new_hkey = "success-" + new_hkey;
+                                    client.rename(new_hkey, suc_new_hkey, function (err, res) {});
                                     console.log(suc_new_hkey);
                                 }
                             });
@@ -56,7 +64,6 @@ if(serverInstance.isGenerator()){
             });
         }
     });
-
 }
 
 
