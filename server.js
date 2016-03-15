b97105d020e01651969e4347944f8a78fa609d12
evgeniy <evgeniy@takeit.to>
2016-03-15 14:05:41 +0200

    switch to generator from worker when generator is unavailiable

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const config = require('config');
 const Instance = require('./instance');
 const async = require('async');
 const redis = require("redis");
-
 let argv = require('minimist')(process.argv.slice(1));
 
 let client = redis.createClient();
@@ -15,97 +14,116 @@ client.on("error", function (err) {
 
 let serverInstance = new Instance(2);
 
-if(serverInstance.isGenerator()){
-    let generatorInfo = { "serverId": serverInstance.serverId,
-                          "timestamp": new Date().getTime().toString(),
-                          "isRunning": "1"
-    };
 
-    client.HMSET("generator-info", generatorInfo, function(err, res){
+function generateMsgs() {
+    if (!serverInstance.isGenerator()){
+        return false
+    }
+    let generatorInfo = {
+        "serverId": serverInstance.serverId,
+        "timestamp": new Date().getTime().toString(),
+        "lastMsgtimestamp": new Date().getTime().toString(),
+        "isRunning": "1"
+    };
+    client.HMSET("generator-info", generatorInfo, function (err, res) {
         if (!err) {
-            let timerMessage = setInterval(function() {
-                let messageBody = { "serverId": serverInstance.serverId,
+            let timerMessage = setInterval(function () {
+                let messageBody = {
+                    "serverId": serverInstance.serverId,
                     "timestamp": new Date().getTime().toString(),
                     "body": serverInstance.getMessage().toString()
                 };
-                console.log(messageBody.body);
+                client.hset("generator-info", "lastMsgtimestamp", new Date().getTime().toString());
+                console.log("message-body: " + messageBody.body);
                 let messageKey = "messageKey-" + serverInstance.serverId + '-' + new Date().getTime();
-
-                client.HMSET(messageKey, messageBody, function(err, res){
+                client.HMSET(messageKey, messageBody, function (err, res) {
                 });
             }, config.messageDelay);
         }
     });
-}else {
-    function handlePackageMsg(){
-        async.waterfall([
-            function (callback) {
-                client.keys("messageKey*", function (err, replies) {
-                    if (!err) {
-                        callback(null, replies);
-                    } else {
+};
+
+function handlePackageMsg(){
+    if (serverInstance.isGenerator()){
+        return false
+    }
+    async.waterfall([
+        function(callback){
+            client.hgetall("generator-info", function (err, res) {
+                if (!err) {
+                    if(((new Date().getTime()) - res.lastMsgtimestamp) > config.waitingForGenerator){
+                        serverInstance.defineAsGenerator();
+                        generateMsgs();
                         callback(true);
                     }
-                });
-            },
-            function (replies, callback) {
-                for (let hkey of replies) {
-                    let new_hkey = "lock-" + hkey;
-                    client.rename(hkey, new_hkey, function (err, res) {
-                        if (!err) {
-                            callback(null, new_hkey);
-                        }
-                    });
+
+                    callback(null);
+                } else {
+                    callback(true);
                 }
-                callback(true);
-            },
-            function (new_hkey, callback) {
-                client.hgetall(new_hkey, function (err, res) {
-                    let message = res;
+            });
+        },
+        function (callback) {
+            client.keys("messageKey*", function (err, replies) {
+                if (!err) {
+                    callback(null, replies);
+                } else {
+                    callback(true);
+                }
+            });
+        },
+        function (replies, callback) {
+            for (let hkey of replies) {
+                let new_hkey = "lock-" + hkey;
+                client.rename(hkey, new_hkey, function (err, res) {
                     if (!err) {
-                        callback(null, message, new_hkey);
-                    } else {
-                        console.log(true);
+                        callback(null, new_hkey);
                     }
                 });
-            },
-            function (message, new_hkey, callback) {
-                serverInstance.eventHandler(message, function (err, msg) {
-                    console.log(msg.body);
-                    if (!err) {
-                        callback(null, new_hkey, "success-" + new_hkey);
-                    } else {
-                        callback(null, new_hkey, "error-" + new_hkey);
-                    }
-                });
-            },
-            function (new_hkey, res_new_hkey, callback) {
-                client.rename(new_hkey, res_new_hkey, function (err, res) {
-                    if (!err) {
-                        callback(null, res)
-                    } else {
-                        callback(true);
-                    }
-                });
+            }
+            callback(true);
+        },
+        function (new_hkey, callback) {
+            client.hgetall(new_hkey, function (err, res) {
+                let message = res;
+                if (!err) {
+                    callback(null, message, new_hkey);
+                } else {
+                    console.log(true);
+                }
+            });
+        },
+        function (message, new_hkey, callback) {
+            serverInstance.eventHandler(message, function (err, msg) {
+                console.log("handle-msg-body: " + msg.body);
+                if (!err) {
+                    callback(null, new_hkey, "success-" + new_hkey);
+                } else {
+                    callback(null, new_hkey, "error-" + new_hkey);
+                }
+            });
+        },
+        function (new_hkey, res_new_hkey, callback) {
+            client.rename(new_hkey, res_new_hkey, function (err, res) {
+                if (!err) {
+                    callback(null, res)
+                } else {
+                    callback(true);
+                }
+            });
+        },
 
-            },
-
-        ], function (err, result) {
-            handlePackageMsg();
-        });
-    };
+    ], function (err, result) {
+        handlePackageMsg();
+    });
+};
 
 
+if(serverInstance.isGenerator()){
+    generateMsgs();
+}else {
     handlePackageMsg();
 
-
-
-
-
 }
-
-
-
-
 
 console.log(serverInstance);
